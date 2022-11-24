c2bd34f56be92407d9a81434bf8df187d1faa8fb
Emanuele213 <vittorius213@gmail.com>
2022-11-24 15:42:29 +0100

    first commit setting header, main and footer

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>
+      <img src="@/img/dc-logo.png" alt="logo dc">
+    </div>
+    <div>
+      <ul>
+        <li>CHARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOPS</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>contenet goes here</h1>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
